51523e1f4134dc91c2c54b4fd919ad7d2fb8de5e
notGonim <mhmodgonim96@gmail.com>
2021-04-14 09:16:15 +0200

    Just practice nothing fancy here to show

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/posts/1';
 //to make an http req to get data back 
 axios_1["default"].get(url).then(function (res) {
-    console.log(res.data);
+    var posts = res.data;
+    var id = posts.id;
+    var title = posts.title;
+    var body = posts.body;
+    logData(id, title, body);
 });
+var logData = function (id, title, body) {
+    console.log("\n    PostID : " + id + "\n    PostTitle : " + title + " \n    PostBody : " + body + "\n    ");
+};
--- index.ts
+++ index.ts
@@ -3,7 +3,28 @@ import axios from 'axios'
 const url ='https://jsonplaceholder.typicode.com/posts/1'
 
 
+//define the data that post gunna hold and get from the Api
+interface Post {
+    id:number ;
+    title:string;
+    body:string;
+}
+
+
 //to make an http req to get data back 
 axios.get(url).then(res=>{
-    console.log(res.data)
+    const posts = res.data  as Post
+    const id = posts.id
+    const title = posts.title
+    const body=posts.body
+ 
+    logData(id,title,body)
 })
+
+const logData=(id:number,title:string,body:string)=>{
+    console.log(`
+    PostID : ${id}
+    PostTitle : ${title} 
+    PostBody : ${body}
+    `)
+}
